f9430266ae43d2637dab6831e0d6ab81f0ce36b2
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-10-25 10:18:44 +0100

    i added member repository

---
+++ src/main/java/com/javangers/repository/MembreRepository.java
@@ -0,0 +1,9 @@
+package com.javangers.repository;
+
+import com.javangers.entity.Membre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MembreRepository extends JpaRepository<Membre, Long> {
+}
